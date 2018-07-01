0cd02410b0053039bb869b8612960fa8e2ef3211
munemu82 <munemu82@gmail.com>
2018-07-01 19:06:51 +1000

    Login page update

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,7 +7,9 @@ export const LoginPage = ({ startLogin }) => (
     <div className="box-layout__box">
       <h1 className="box-layout__title">WestX Shuttlebus</h1>
       <p>Login to make or access your bookings.</p> 
-             <button className="btn btn-secondary btn-lg btn-block"  onClick={startLogin}> Login </button>
+             <button className="btn btn-secondary btn-lg btn-block"  onClick={startLogin}> Login with Google</button>
+             <button className="btn btn-secondary btn-lg btn-block"> Login with Facebook</button>
+   
     </div>
   </div>
 );
